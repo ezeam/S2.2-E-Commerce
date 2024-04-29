7d3ac9e0348f71bff7c7aaf1b0d7aadefb841351
Ester <ester.zea.maldonado@gmail.com>
2024-04-29 18:56:35 +0200

    WIP-acabando el 4

--- js/shop.js
+++ js/shop.js
@@ -70,48 +70,71 @@ var products = [
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [
-    
-];
-
+var cart = [{quantity: 2, id: 9, name: 'Toddler Frock', price: 9.99, type: 'clothes'}, {quantity: 1, id: 7, name: 'Lawn Dress', price: 15, 
+type: 'clothes'}, {quantity: 4, id: 6, name: 'Lip Tints', price: 12.75, type: 'beauty'}, {quantity: 3, id: 1, name: 'cooking oil', price: 10.5,
+type: 'grocery', offer: {number: 3, percent: 20}}, {quantity: 11, id: 3, name: 'Instant cupcake mixture', price: 5, type: 'grocery',offer: {
+number: 10, percent: 30}}];
+/*Datos de prueba {quantity: 2, id: 9, name: 'Toddler Frock', price: 9.99, type: 'clothes'}, {quantity: 1, id: 7, name: 'Lawn Dress', price: 15, 
+type: 'clothes'}, {quantity: 4, id: 6, name: 'Lip Tints', price: 12.75, type: 'beauty'}, {quantity: 3, id: 1, name: 'cooking oil', price: 10.5,
+type: 'grocery', offer: {number: 3, percent: 20}}, {quantity: 11, id: 3, name: 'Instant cupcake mixture', price: 5, type: 'grocery',offer: {
+number: 10, percent: 30}} 
+*/
 var total = 0;
+var totalCartQuantity = 0;
 
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     let found = products.find(product => product.id === id);
-    console.log("Objeto que queremos añadir al carrito: ", found);
+    //console.log("Objeto que queremos añadir al carrito: ", found);
     // 2. Add found product to the cart array
     let indice = cart.findIndex((producto) => producto.id == id);
     if(indice == -1){
-        console.log("NO he encontrado un objeto con el mismo id en cart");
+        //console.log("NO he encontrado un objeto con el mismo id en cart");
         found = Object.assign({quantity: 1}, found);
-        console.log("Objeto con la quantity añadida", found);
+        //console.log("Objeto con la quantity añadida", found);
         cart.push(found);
         console.log("Array cart con quantity", cart);
     }else{        
         cart[indice].quantity++;
-        console.log("He encontrado un objeto con el mismo id en cart", cart);
+        //console.log("He encontrado un objeto con el mismo id en cart", cart);
     }  
 }
 
 // Exercise 2
 function cleanCart() {
-    cart = [
-    
-    ];
-    console.log(cart); //NO DESAPARECEN LOS ARTÍCULOS DEL CARRITO, PERO SI QUE VACÍA LA ARRAY Y LA INICIALIZA
+    cart.length = 0;
+    total = 0;
+    document.getElementById("cart_list").innerHTML = '';
+    document.getElementById('total_price').textContent = 0;
+    document.getElementById('count_product').textContent = totalCartQuantity;
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    let total = cart.reduce((acumulador, producto) => acumulador + producto.price, 0);
+    return total; 
 }
+//console.log(calculateTotal());
 
 // Exercise 4
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    let subtotalWithDiscount = 0;
+    for (let i = 0; i < cart.length; i++){
+        //console.log(cart[i]);
+        if(cart[i].id == 1 && cart[i].quantity >= 3){
+            subtotalWithDiscount = (cart[i].price) * cart[i].quantity * 0.20;
+            console.log("Primer if", subtotalWithDiscount);
+        }
+        if(cart[i].id == 3 && cart[i].quantity >= 10){
+            subtotalWithDiscount = (cart[i].price) * cart[i].quantity * 0.30;
+            console.log("Segundo if",subtotalWithDiscount);
+        }
+    }    
 }
+applyPromotionsCart();
 
 // Exercise 5
 function printCart() {
@@ -129,3 +152,10 @@ function removeFromCart(id) {
 function open_modal() {
     printCart();
 }
+
+
+//Custom functions
+
+function updateCountProduct() {
+    document.getElementById('count_product').textContent = totalCartQuantity;
+  }
